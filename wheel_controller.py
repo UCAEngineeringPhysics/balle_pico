f8c6b40f93439323a1d5e676b6cd36a64045461f
linzhanguca <lzhang12@uca.edu>
2025-11-06 23:33:04 -0600

    update pid controller

--- wheel_controller.py
+++ wheel_controller.py
@@ -6,70 +6,77 @@ class WheelController(WheelDriver):
     def __init__(self, driver_ids, encoder_ids):
         # Pin configuration
         super().__init__(driver_ids, encoder_ids)  # call super class's "__init__"
-        self.vel_executor = Timer(
-            mode=Timer.PERIODIC, freq=30, callback=self.regulate_velocity
-        )
+        # Constants
+        self.k_p = 0.55
+        self.k_i = 0.02
+        self.k_d = 0.4
+        self.freq_reg = 50  # Hz
         # Variables
-        self.target_vel = 0.0
-        self.err = 0.0
-        self.prev_err = 0.0
-        self.acc_err = 0.0
-        self.diff_err = 0.0
-        self.duty = 0
-        self.set_vel_counter = 0
-        # Properties: Incremental PID
-        self.K_P = 17600.0
-        self.K_I = 0.0
-        self.K_D = 0.0
+        self.reg_vel_counter = 0
+        self.duty = 0.0
+        self.error = 0.0
+        self.prev_error = 0.0
+        self.error_inte = 0.0  # integral
+        self.error_diff = 0.0  # differentiation
+        self.ref_lin_vel = 0.0
+        # PID controller config
+        self.vel_reg_timer = Timer(
+            freq=self.freq_reg,
+            mode=Timer.PERIODIC,
+            callback=self.regulate_velocity,
+        )
 
     def regulate_velocity(self, timer):
-        self.err = self.target_vel - self.lin_vel
-        self.acc_err += self.err  # err_sum = err_sum + err
-        self.diff_err = self.err - self.prev_err
-        self.prev_err = self.err
-        inc_duty = (
-            self.K_P * self.err + self.K_I * self.acc_err + self.K_D * self.diff_err
-        )
-        self.duty += inc_duty
-        if self.duty > 0:  # forward
-            if self.duty > 65535:
-                self.duty = 65535
-            self.forward(int(self.duty))
-        elif self.duty < 0:  # backward
-            if self.duty < -65535:
-                self.duty = -65535
-            self.backward(int(-self.duty))  # self.duty is negative
+        if self.ref_lin_vel == 0.0 or self.reg_vel_counter > self.freq_reg:
+            self.stop()
+            self.prev_error = 0.0
         else:
-            self.stop()
-        # No need for PID control if target velocity is 0
-        if self.target_vel == 0:
-            self.stop()
-        self.set_vel_counter += 1
+            self.error = self.ref_lin_vel - self.meas_lin_vel  # ang_vel also works
+            self.error_inte += self.error
+            self.error_diff = self.error - self.prev_error
+            self.prev_error = self.error  # UPDATE previous error
+            inc_duty = (
+                self.k_p * self.error
+                + self.k_i * self.error_inte
+                + self.k_d * self.error_diff
+            )
+            self.duty = self.duty + inc_duty
+            if self.duty > 0:
+                if self.duty > 1.0:
+                    self.duty = 1.0
+                self.forward(self.duty)
+            else:
+                if self.duty < -1.0:
+                    self.duty = -1.0
+                self.backward(-self.duty)
+            self.reg_vel_counter += 1
 
-    def set_lin_vel(self, target_vel):
-        """
-        Set a reference LINEAR VELOCITY for this wheel
-        """
-        if target_vel is not self.target_vel:
-            self.target_vel = target_vel
-            self.acc_err = 0.0
-            self.set_vel_counter = 0
-
+    def set_wheel_velocity(self, ref_lin_vel):
+        if ref_lin_vel is not self.ref_lin_vel:
+            self.ref_lin_vel = ref_lin_vel
+            self.prev_error = 0.0
+            self.error_inte = 0.0
+            self.reg_vel_counter = 0
 
 
 # TEST
 if __name__ == "__main__":
-    from time import sleep
+    from utime import sleep
+    from machine import Pin
 
-    # w = WheelController((2, 3, 4), (21, 20))  # r
-    w = WheelController((6, 7, 8), (11, 10))  # l
-    print(f"target velocity: {w.target_vel}")
-    for v in range(1, 11):
-        w.set_lin_vel(-v / 10)
-        sleep(1.5)
-        print(f"target velocity: {w.target_vel}, actual velocity: {w.lin_vel}")
-    for v in reversed(range(10)):
-        w.set_lin_vel(-v / 10)
-        sleep(1.5)
-        print(f"target velocity: {w.target_vel}, actual velocity: {w.lin_vel}")
-    w.set_lin_vel(0)
+    wc = WheelController(
+        driver_ids=(6, 7, 8),
+        encoder_ids=(11, 10),
+    )
+    for i in range(100):
+        if i == 24:  # step up @ t=0.5 s
+            wc.set_wheel_velocity(0.7)
+        elif i == 74:  # step down @ t=1.5 s
+            wc.set_wheel_velocity(0.0)
+        print(
+            f"Reference velocity={wc.ref_lin_vel} m/s, Measured velocity={wc.meas_lin_vel} m/s"
+        )
+        sleep(0.02)
+    # Terminate
+    wc.disable()
+    print("Wheel disabled")
