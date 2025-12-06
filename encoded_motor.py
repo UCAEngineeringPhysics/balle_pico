6d7afe2da8be3fd73e9eec58535948f6998c8df3
linzhanguca <lzhang12@uca.edu>
2025-12-06 17:45:35 -0600

    add reset_counts()

--- encoded_motor.py
+++ encoded_motor.py
@@ -9,8 +9,8 @@ class EncodedMotor(BaseMotor):
         self.enca_pin = Pin(encoder_ids[0], Pin.IN)
         self.encb_pin = Pin(encoder_ids[1], Pin.IN)
         self.enca_pin.irq(trigger=Pin.IRQ_RISING, handler=self.update_counts)
-        # Variables
-        self.encoder_counts = 0
+        # zero out encoder counts
+        self.reset_counts()
 
     def update_counts(self, pin):
         if self.encb_pin.value() == pin.value():  # A channel RISE later than B channel
@@ -18,20 +18,23 @@ class EncodedMotor(BaseMotor):
         else:
             self.encoder_counts += 1
 
+    def reset_counts(self):
+        self.encoder_counts = 0
+
 
 # TEST
 if __name__ == "__main__":  # Test only the encoder part
     from utime import sleep
 
     # SETUP
-    em = EncodedMotor(
-        driver_ids=(16, 18, 17),
-        encoder_ids=(20, 19),
-    )  # left motor, encoder's green and yellow on GP19 and GP20
     # em = EncodedMotor(
-    #     driver_ids=(15, 13, 14),
-    #     encoder_ids=(10, 11),
-    # )  # right motor, encoder's green and yellow on GP10 and GP11
+    #     driver_ids=(16, 18, 17),
+    #     encoder_ids=(20, 19),
+    # )  # left motor, encoder's green and yellow on GP19 and GP20
+    em = EncodedMotor(
+        driver_ids=(15, 13, 14),
+        encoder_ids=(10, 11),
+    )  # right motor, encoder's green and yellow on GP10 and GP11
 
     # LOOP
     # Forward ramp up and down
