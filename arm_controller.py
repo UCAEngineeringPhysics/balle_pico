f008de167a4c7c41bc4c2ff018e3ce2c9b48b251
linzhanguca <lzhang12@uca.edu>
2025-12-10 00:50:23 -0600

    remove timer

--- arm_controller.py
+++ arm_controller.py
@@ -1,4 +1,4 @@
-from machine import Pin, PWM, Timer
+from machine import Pin, PWM
 from time import sleep
 
 SHOULDER_NEUTRAL = 1_400_000
@@ -17,50 +17,26 @@ class ArmController:
         self.left_shoulder.freq(50)
         self.right_shoulder = PWM(Pin(right_shoulder_id))
         self.right_shoulder.freq(50)
-        # Config controller timer
-        self.jp_reg_timer = Timer(
-            freq=25,
-            mode=Timer.PERIODIC,
-            callback=self.regulate_joints,
-        )
         # Variables
         self.claw_pulse_width = CLAW_MID
-        self.targ_pos_claw = 0
-        self.pos_inc = 0.2
         # Constants
         self.claw.duty_ns(CLAW_MID)
         self.pw_inc = 50_000
 
-    def regulate_joints(self, timer):
-        err_claw = self.targ_pos_claw - self.claw_pos
-        if err_claw > 0:
-            self.claw_pos += self.pos_inc
-            if self.claw_pos > 1:
-                self.claw_pos = 1
-        elif err_claw < 0:
-            self.claw_pos -= self.pos_inc
-            if self.claw_pos < -1:
-                self.claw_pos = -1
-        else:
-            self.claw_pos = self.targ_pos_claw
-        self.claw.duty_ns(CLAW_MID + int(self.claw_pos * CLAW_RANGE))
+    def close_claw(self, dir):  # Close claw
+        """
+        Open/Close claw
+        Args:
+            dir: -1/0/1, open/maintain/close
+        """
+        assert dir == 0 or dir == -1 or dir == 1
+        self.claw_pulse_width += self.pw_inc * dir
+        if self.claw_pulse_width >= CLAW_MAX:
+            self.claw_pulse_width = CLAW_MAX
+        elif self.claw_pulse_width <= CLAW_MIN:
+            self.claw_pulse_width = CLAW_MIN
+        self.claw.duty_ns(self.claw_pulse_width)
 
-    def set_joint_positions(self, targ_pos_claw):
-        assert -1 <= targ_pos_claw <= 1
-        self.targ_pos_claw = targ_pos_claw
-
-    # def close_claw(self, dir):  # Close claw
-    #     """
-    #     Open/Close claw
-    #     Args:
-    #         dir: -1/0/1, open/maintain/close
-    #     """
-    #     self.claw_pulse_width += self.pw_inc * dir
-    #     if self.claw_pulse_width >= CLAW_MAX:
-    #         self.claw_pulse_width = CLAW_MAX
-    #     elif self.claw_pulse_width <= CLAW_MIN:
-    #         self.claw_pulse_width = CLAW_MIN
-    #     self.claw.duty_ns(self.claw_pulse_width)
     #
 
 
@@ -70,4 +46,4 @@ if __name__ == "__main__":
 
     sleep(1)
     ac = ArmController(2, 3, 4)
-    ac.set_joint_positions(0)
+    ac.close_claw(0)
