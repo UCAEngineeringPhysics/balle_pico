67bb2f324d6749fd158838247061ab329f04aa8d
ericlejune <elejune@cub.uca.edu>
2026-04-03 21:52:23 -0500

    start timer dev

--- upython_scripts/manipulation/arm_controller.py
+++ upython_scripts/manipulation/arm_controller.py
@@ -1,4 +1,4 @@
-from machine import Pin, PWM
+from machine import Pin, PWM, Timer
 from time import sleep
 
 
@@ -12,48 +12,63 @@ CLAW_MIN = 1_550_000
 
 class ArmController:
     # TODO: timer for setting target positions
-    def __init__(self, claw_pin, arm_pin_a, arm_pin_b):
-        self.claw_servo = PWM(Pin(claw_pin))
-        self.shoulder_servo_a = PWM(Pin(arm_pin_a))
-        self.shoulder_servo_b = PWM(Pin(arm_pin_b))
-        self.claw_servo.freq(50)
-        self.shoulder_servo_a.freq(50)
-        self.shoulder_servo_b.freq(50)
+    def __init__(self, claw_pin, shoulder_a_pin, shoulder_b_pin):
+        self.claw = PWM(Pin(claw_pin))
+        self.shoulder_a = PWM(Pin(shoulder_a_pin))
+        self.shoulder_b = PWM(Pin(shoulder_b_pin))
+        self.claw.freq(50)
+        self.shoulder_a.freq(50)
+        self.shoulder_b.freq(50)
         # Set initial positions
-        self.set_neutral()
+        # self.set_neutral()
+        self.joints_set_timer = Timer(
+            freq=100,
+            mode=Timer.PERIODIC,
+            callback=self.set_joint_positions,
+        )
+        # Variables
+        self.claw_target = CLAW_NEUTRAL
+        self.shoulder_a_target = SHOULDER_NEUTRAL
+        self.shoulder_b_target = SHOULDER_NEUTRAL
 
-    def set_neutral(self):
-        self.shoulder_servo_a.duty_ns(SHOULDER_NEUTRAL)
-        self.shoulder_servo_b.duty_ns(SHOULDER_NEUTRAL)
-        self.claw_servo.duty_ns(CLAW_NEUTRAL)
-        # Save neutral value as current value
-        self.shoulder_duty_a = SHOULDER_NEUTRAL
-        self.shoulder_duty_b = SHOULDER_NEUTRAL
-        self.claw_duty = CLAW_NEUTRAL
+    def set_joint_positions(self, timer):
+        self.claw.duty_ns(self.claw_target)
+        self.shoulder_a.duty_ns(self.shoulder_a_target)
+        self.shoulder_b.duty_ns(self.shoulder_b_target)
 
-    def lower_claw(self, dc_inc=0):  # negtive dc_inc to raise arm
-        assert -50_000 <= dc_inc <= 50_000  # recommend dc increment: <20_000 ns
-        self.shoulder_duty_a += dc_inc
-        self.shoulder_duty_b -= dc_inc
-        # Lowest
-        if self.shoulder_duty_a >= 2_400_000:
-            self.shoulder_duty_a = 2_400_000
-            self.shoulder_duty_b = 700_000
-        # Highest
-        elif self.shoulder_duty_a <= 700_000:
-            self.shoulder_duty_a = 700_000
-            self.shoulder_duty_b = 2_700_000
-        self.shoulder_servo_a.duty_ns(self.shoulder_duty_a)
-        self.shoulder_servo_b.duty_ns(self.shoulder_duty_b)
-
-    def close_claw(self, dc_inc=0):
-        assert -50_000 <= dc_inc <= 50_000
-        self.claw_duty += dc_inc
-        if self.claw_duty >= 2_500_000:  # 2_500_000
-            self.claw_duty = 2_500_000
-        elif self.claw_duty <= 1_550_000:
-            self.claw_duty = 1_550_000
-        self.claw_servo.duty_ns(self.claw_duty)
+    # def set_neutral(self):
+    #     self.shoulder_servo_a.duty_ns(SHOULDER_NEUTRAL)
+    #     self.shoulder_servo_b.duty_ns(SHOULDER_NEUTRAL)
+    #     self.claw_servo.duty_ns(CLAW_NEUTRAL)
+    #     # Save neutral value as current value
+    #     self.shoulder_duty_a = SHOULDER_NEUTRAL
+    #     self.shoulder_duty_b = SHOULDER_NEUTRAL
+    #     self.claw_duty = CLAW_NEUTRAL
+    #
+    # def lower_claw(self, dc_inc=0):  # negtive dc_inc to raise arm
+    #     assert -50_000 <= dc_inc <= 50_000  # recommend dc increment: <20_000 ns
+    #     self.shoulder_duty_a += dc_inc
+    #     self.shoulder_duty_b -= dc_inc
+    #     # Lowest
+    #     if self.shoulder_duty_a >= 2_400_000:
+    #         self.shoulder_duty_a = 2_400_000
+    #         self.shoulder_duty_b = 700_000
+    #     # Highest
+    #     elif self.shoulder_duty_a <= 700_000:
+    #         self.shoulder_duty_a = 700_000
+    #         self.shoulder_duty_b = 2_700_000
+    #     self.shoulder_servo_a.duty_ns(self.shoulder_duty_a)
+    #     self.shoulder_servo_b.duty_ns(self.shoulder_duty_b)
+    #
+    # def close_claw(self, dc_inc=0):
+    #     assert -50_000 <= dc_inc <= 50_000
+    #     self.claw_duty += dc_inc
+    #     if self.claw_duty >= 2_500_000:  # 2_500_000
+    #         self.claw_duty = 2_500_000
+    #     elif self.claw_duty <= 1_550_000:
+    #         self.claw_duty = 1_550_000
+    #     self.claw_servo.duty_ns(self.claw_duty)
+    #
 
 
 # Example usage
