59c835157db331f46409701fd2ccbec204839b57
ericlejune <elejune@cub.uca.edu>
2026-04-04 11:44:48 -0500

    stuck at the moment, cannot close claw

--- upython_scripts/manipulation/arm_controller.py
+++ upython_scripts/manipulation/arm_controller.py
@@ -8,6 +8,7 @@ SHOULDER_MIN = 700_000  # nano sec
 CLAW_NEUTRAL = 1_800_000
 CLAW_MAX = 2_500_000
 CLAW_MIN = 1_550_000
+PULSE_WIDTH_INC_STEP = 10_000
 
 
 class ArmController:
@@ -19,22 +20,39 @@ class ArmController:
         self.claw.freq(50)
         self.shoulder_a.freq(50)
         self.shoulder_b.freq(50)
-        # Set initial positions
-        # self.set_neutral()
+        # Set to neutral
+        self.claw.duty_ns(CLAW_NEUTRAL)
+        self.shoulder_a.duty_ns(SHOULDER_NEUTRAL)
+        self.shoulder_b.duty_ns(SHOULDER_NEUTRAL)
+        # Variables
+        self.pulse_width_clw = CLAW_NEUTRAL
+        self.pulse_width_sha = SHOULDER_NEUTRAL
+        self.pulse_width_shb = SHOULDER_NEUTRAL
+        self.target_pulse_width_clw = CLAW_NEUTRAL
+        self.target_pulse_width_sha = SHOULDER_NEUTRAL
+        self.target_pulse_width_shb = SHOULDER_NEUTRAL
+        # Set joint pos timer
         self.joints_set_timer = Timer(
             freq=100,
             mode=Timer.PERIODIC,
-            callback=self.set_joint_positions,
+            callback=self.manipulate_joints,
         )
-        # Variables
-        self.claw_target = CLAW_MAX
-        self.shoulder_a_target = SHOULDER_NEUTRAL
-        self.shoulder_b_target = SHOULDER_NEUTRAL
 
-    def set_joint_positions(self, timer):
-        self.claw.duty_ns(self.claw_target)
-        self.shoulder_a.duty_ns(self.shoulder_a_target)
-        self.shoulder_b.duty_ns(self.shoulder_b_target)
+    def set_joint_positions(
+        self,
+        target_pulse_width_clw=CLAW_NEUTRAL,
+        target_pulse_width_sha=SHOULDER_NEUTRAL,
+    ):
+        self.target_pulse_width_clw = target_pulse_width_clw
+        self.target_pulse_width_sha = target_pulse_width_sha
+        self.target_pulse_width_shb = SHOULDER_NEUTRAL - (
+            target_pulse_width_sha - SHOULDER_NEUTRAL
+        )
+
+    def manipulate_joints(self, timer):
+        diff_pulse_width_clw = self.target_pulse_width_clw - self.pulse_width_clw
+        self.pulse_width_clw = max(min(diff_pulse_width_clw, PULSE_WIDTH_INC_STEP), -PULSE_WIDTH_INC_STEP)
+        self.claw.duty_ns(self.pulse_width_clw)
 
     # def set_neutral(self):
     #     self.shoulder_servo_a.duty_ns(SHOULDER_NEUTRAL)
@@ -75,8 +93,11 @@ class ArmController:
 if __name__ == "__main__":
     from utime import sleep
 
-    sleep(1)
     ac = ArmController(15, 13, 14)
+    sleep(1)
+    ac.set_joint_positions(CLAW_NEUTRAL, SHOULDER_NEUTRAL)
+    sleep(10)
+
 #     for _ in range(80):
 #         ac.close_claw(10_000)
 #         sleep(0.1)
